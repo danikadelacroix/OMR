f490f2bb55d3b1b2bb92bad49cd62adbb397c095
AnushkaRajput <161483729+danikadelacroix@users.noreply.github.com>
2025-06-11 02:11:24 +0530

    Update db_utlis.py

--- db_utlis.py
+++ db_utlis.py
@@ -1,5 +1,22 @@
 import sqlite3
 
+def init_db():
+    conn = sqlite3.connect("omr_results.db")
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS results (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            student_id TEXT,
+            answers TEXT,
+            correct_answers TEXT,
+            score INTEGER,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+
 def store_result(student_id, answers, correct_answers, score):
     conn = sqlite3.connect("omr_results.db")
     cursor = conn.cursor()
